48fbde58d66f16e6f72e4f31ef13747000268231
Mark Blyth <blyth87@gmail.com>
2020-04-24 12:19:28 +0100

    Add markdown mode

--- init.el
+++ init.el
@@ -323,6 +323,14 @@
   "pf" 'elpy-folding-toggle-at-point
 )
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Config
